8e8dab16019ec3f640a1b84b12894204bf5f5d97
manojdivakar69bit <manojdivakar69@gmail.com>
2026-04-09 22:03:58 +0530

    Update EmergencyPage.tsx

--- src/pages/EmergencyPage.tsx
+++ src/pages/EmergencyPage.tsx
@@ -144,8 +144,7 @@ const EmergencyPage = () => {
         : `📍 *Location:* GPS unavailable\n🏠 *Address:* ${data?.customer?.address || "N/A"}`;
 
       const msg =
-`🚨 *ACCIDENT ALERT FROM CALL MY FAMILY!*
- 🌎 callmyfamily.in
+`🚨 *ACCIDENT ALERT FROM 🌎callmyfamily.in!*
 
 👤 *Name:* ${data?.customer?.name || "Unknown"}
 🚗 *Vehicle:* ${data?.customer?.vehicle_number || "N/A"}
